d07821e37344e08821694ae138bc60e9e674cb2a
PhsTutors <phsefamilia8@gmail.com>
2022-04-03 23:42:11 -0300

    Cria tod documentação do sistema de Login

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [UsersController::class, 'index'])->name('users.index');
+Route::get('/logar', [UsersController::class, 'logar'])->name('users.logar');
+Route::post('/store', [UsersController::class, 'store'])->name('store');
+Route::post('/login', [UsersController::class, 'login'])->name('login');
